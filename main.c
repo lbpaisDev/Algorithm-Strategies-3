efe119588ecad1cdadefd9237728620b051832d9
Afonso Magalhães <afonsomm98@gmail.com>
2021-04-14 15:53:40 +0100

    passar para top-down

--- main.c
+++ main.c
@@ -1,7 +1,12 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 #define MODVAL 1000000007
+
+int H;
+int h;
+int n;
 
 int mod_abs(int a, int mod)
 {
@@ -18,10 +23,84 @@ int mod_sub(int a, int b, int mod)
     return mod_add(a, -b, mod);
 }
 
-int build_arc(int n, int h, int H)
+int accept(int x,int y)
 {
+    if(x == 1 && y == 0) // rejection test 2: se y = 0 na segunda posição horizontal da sala então k < 3 (regra 1)
+    {
+        return 0;
+    }
+
+    if(y >= (n - x) * h - 1) // rejection test 3: se já não houver espaço suficiente para os blocos chegarem a y = 0 até ao fim da sala (regra 2)
+    {
+        return 0;
+    }
+
+    return 1;
+}
+
+void getlimits(int limits[2], int y, int d) {
+    int init, end;
+
+    if(y > h - 1) // se o bloco estiver numa altura superior a h - 1 
+    {
+        init = y - h + 1;
+    }
+    else
+    {
+        init = 0;
+    }
+    limits[0] = init;
+
+    if(d == -1 || H - y == h) // se os blocos estão a descer ou chegou à máxima altura possível na sala (para a altura do bloco)
+    {
+        end = y - 1;
+    }
+    else if(H - y < 2 * h)
+    {
+        end = H - h;
+    }
+    else
+    {
+        end = y + h - 1;
+    }
+
+    limits[1] = end;
+}
+
+int arcs(int x, int y, int d)
+{
+    if(accept(x, y) == 0) // testes de rejeição
+    {
+        return 0;
+    }
     
-    return 0;
+    if(x > 1 && y == 0) // teste de aceitação
+    {
+        return 1;
+    }
+
+    int limits[2];
+    getlimits(limits, y, d); // calcular limites de posições do próximo bloco de modo a que os blocos atual e seguinte partilhem pelo menos 1 de altura (regra 3)
+    
+    int sols = 0;
+    for(int i = limits[0]; i <= limits[1]; i++)
+    {
+        if(i == y) // se a altura do próximo bloco for igual à do atual, rejeitar (regra 4)
+        {
+            continue;
+        }
+
+        if(i < y) // se a altura do próximo bloco for inferior à do atual, então chamar a função recursiva atualizando x para x + 1, y para i e direção para -1
+        {
+            sols += arcs(x+1, i, -1);
+        }
+        else // se a altura do próximo bloco for superior à do atual, então chamar a função recursiva atualizando x para x + 1, y para i e direção para 1
+        { 
+            sols += arcs(x+1, i, 1);
+        }
+    }
+
+    return sols;
 }
 
 int main()
@@ -29,10 +108,12 @@ int main()
     int t;
     scanf("%d", &t);
 
-    int n, h, H;
-    for (int i = 0; i < t; i++)
+    while (t > 0)
     {
         scanf("%d %d %d", &n, &h, &H);
+        printf("%d\n", mod_abs(arcs(0, 0, 1), 1000000007));
+
+        t--;
     }
 
     return 0;
