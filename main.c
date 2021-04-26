5f6cfb21e1bfe561a3d87170abcae258e813689f
Afonso Magalhães <afonsomm98@gmail.com>
2021-04-26 20:49:32 +0100

    corrigir otimizacao

--- main.c
+++ main.c
@@ -1,6 +1,5 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <string.h>
 
 #define MODVAL 1000000007
 
@@ -24,69 +23,6 @@ int mod_sub(int a, int b, int mod)
     return mod_add(a, -b, mod);
 }
 
-void getlowerblocks(int limits[2], int y, int x)
-{
-    int init, end;
-
-    if (y == 0)
-    {
-        init = -1;
-        end = 0;
-    }
-    else if (y < h - 1)
-    {
-        init = y - 1;
-        end = 1;
-    }
-    else
-    {
-        init = y - 1;
-        end = y - h + 1;
-    }
-
-    if (init - (h - 1) * x > 0)
-    {
-        init = (h - 1) * x;
-    }
-
-    if (end - x < 0)
-    {
-        end = x;
-    }
-
-    limits[0] = init;
-    limits[1] = end;
-}
-
-void getupperblocks(int limits[2], int y, int x)
-{
-    int init, end;
-
-    if (y + h == H)
-    {
-        init = 0;
-        end = -1;
-    }
-    else if (y + 2 * h - 1 > H)
-    {
-        init = y + 1;
-        end = H - h;
-    }
-    else
-    {
-        init = y + 1;
-        end = y + h - 1;
-    }
-
-    if (end - (h - 1) * x > 0)
-    {
-        end = (h - 1) * x;
-    }
-
-    limits[0] = init;
-    limits[1] = end;
-}
-
 int getsols(int sols[2])
 {
     return mod_add(sols[0], sols[1], MODVAL);
@@ -94,55 +30,103 @@ int getsols(int sols[2])
 
 int arcs()
 {
-    // Caso seja possível chegar a metade do valor de n subindo apenas 1 de altura de cada vez, então há pelo menos uma solução para esse n
-    // Caso não dê, não é possível chegar a esse n
+    // ----- Base Case
+    int last_n = 0;
     for (int i = 3; i <= n; i++)
     {
-        if (h + i / 2 <= H)
+        if (h + i / 2 <= H) // Caso seja possível chegar a metade do valor de n subindo apenas 1 de altura de cada vez, então há pelo menos uma solução para esse n
         {
             sols[i - 1][0][1] = 1;
+            sols[i - 2][1][1] = 1;
+            last_n = i;
         }
-    }
-
-    int limits[2];
-
-    // Percorrer matriz do fim ao início
-    for (int i = n - 1; i > 1; i--)
-    {
-        for (int j = 0; j < H; j++)
+        else
         {
-            // Se o index não tiver nenhuma solução e for maior que 0 quer dizer que ja não há mais blocos nesse valor de i (penso eu)
-            if (getsols(sols[i][j]) == 0)
-            {
-                if (j > 0)
-                {
-                    break;
-                }
-                continue;
-            }
-
-            // Ir buscar blocos possíveis anteriores a partir do atual e que estão acima deste
-            getupperblocks(limits, j, i - 1);
-            for (int k = limits[0]; k <= limits[1]; k++)
-            {
-                sols[i - 1][k][1] = mod_add(sols[i - 1][k][1], sols[i][j][1], MODVAL);
-            }
-
-            // Ir buscar blocos possíveis anteriores a partir do atual e que estão abaixo deste
-            getlowerblocks(limits, j, i - 1);
-            for (int k = limits[0]; k >= limits[1]; k--)
-            {
-                sols[i - 1][k][0] = mod_add(sols[i - 1][k][0], getsols(sols[i][j]), MODVAL);
-            }
+            sols[i - 1][0][1] = 0;
+            sols[i - 2][1][1] = 0;
         }
     }
 
-    for (int i = 1; i < h; i++)
+    if (last_n == 0)
+    {
+        return 0;
+    }
+    // -----
+
+    int next_end = 0; // ultimo bloco possivel na coluna seguinte da sala
+    for (int i = n - 2; i > 0; i--)
+    {
+        int end = next_end + h - 1;
+        if (end > (h - 1) * i)
+        {
+            end = (h - 1) * i;
+        }
+        if (end > H - h)
+        {
+            end = H - h;
+        }
+
+        if (i == 1)
+        {
+            int sols_end = h;
+            if (sols_end > next_end)
+            {
+                sols_end = next_end;
+            }
+
+            for (int j = 2; j <= sols_end; j++)
+            {
+                sols[i][j][0] = mod_add(sols[i][j][0], getsols(sols[i + 1][j]), MODVAL);
+            }
+        }
+
+        for (int j = 2; j <= end; j++)
+        {
+            sols[i][j][1] = sols[i][j - 1][1];
+            if (j - 1 <= next_end)
+            {
+                sols[i][j][1] = mod_add(sols[i][j][1], sols[i + 1][j - 1][1], MODVAL);
+            }
+            if (j > h - 1)
+            {
+                sols[i][j][1] = mod_sub(sols[i][j][1], sols[i + 1][j - h][1], MODVAL);
+            }
+
+            sols[i][j][0] = 0;
+            if (j < next_end && j - i >= 0)
+            {
+                if (sols[i][j - 1][0] > 0)
+                {
+                    sols[i][j][0] = mod_sub(sols[i][j - 1][0], getsols(sols[i + 1][j]), MODVAL);
+                    if (j + h - 1 <= next_end)
+                    {
+                        sols[i][j][0] = mod_add(sols[i][j][0], getsols(sols[i + 1][j + h - 1]), MODVAL);
+                    }
+                }
+                else
+                {
+                    int upperblocks_end = j + h - 1;
+                    if (upperblocks_end > next_end)
+                    {
+                        upperblocks_end = next_end;
+                    }
+
+                    for (int k = j + 1; k <= upperblocks_end; k++)
+                    {
+                        sols[i][j][0] = mod_add(sols[i][j][0], getsols(sols[i + 1][k]), MODVAL);
+                    }
+                }
+            }
+        }
+
+        next_end = end;
+    }
+
+    for (int i = 1; i < next_end; i++) // A soma de todas as soluções vai estar no index inicial
     {
         sols[0][0][0] = mod_add(sols[0][0][0], getsols(sols[1][i]), MODVAL);
     }
 
-    // A soma de todas as soluções vai estar no index inicial
     return sols[0][0][0];
 }
 
@@ -154,7 +138,6 @@ int main()
     while (t > 0)
     {
         scanf("%d %d %d", &n, &h, &H);
-        memset(sols, 0, 500 * 60000 * 2 * sizeof(int));
         printf("%d\n", mod_abs(arcs(), MODVAL));
         t--;
     }
